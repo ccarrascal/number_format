412723b2c8db9226a301257088d8ed9607ddf6f1
Charles <charles@totoro3>
2015-10-25 02:32:45 +0100

    Build file upgrades

--- Code/java_format_numbers/src/org/crossover/util/NumberPrettifier.java
+++ Code/java_format_numbers/src/org/crossover/util/NumberPrettifier.java
@@ -17,7 +17,7 @@ import java.text.DecimalFormatSymbols;
 * Short scale numbers are used to output the prettified number.
 *
 * Examples:
-* 
+* <pre>
 *		input: 1000000 output: 1M
 *
 *		input: 2500000.34 output: 2.5M
@@ -25,6 +25,7 @@ import java.text.DecimalFormatSymbols;
 *		input: 532 output: 532
 *
 *		input: 1123456789 output: 1.1B*
+*</pre>
 *
 * @author Carlos Carrascal Sanchez
 *
