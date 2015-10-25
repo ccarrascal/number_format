89d25aff8c88e2db74278efbd00a3a1f1df29de4
Charles <charles@totoro3>
2015-10-25 02:36:05 +0100

    Updated paths

--- Code/java_format_numbers/runtest.sh
+++ Code/java_format_numbers/runtest.sh
@@ -1,2 +1,4 @@
-java -cp bin:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar JunitTestSuiteRunner
+#Test are automatically executed on build, but this script will execute them on demand
+
+java -cp bin:bin/tests:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar JunitTestSuiteRunner
 
