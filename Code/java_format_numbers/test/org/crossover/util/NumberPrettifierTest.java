89de545a2c6c5fdf4032ca97d8adfb1e332b09af
Charles <charles@totoro3>
2015-10-25 02:52:07 +0100

    Added test for negative, wrong domain and more

--- Code/java_format_numbers/test/org/crossover/util/NumberPrettifierTest.java
+++ Code/java_format_numbers/test/org/crossover/util/NumberPrettifierTest.java
@@ -5,12 +5,24 @@ import org.junit.Test;
 import java.util.Locale;
 
 
+/**
+* JUnit test class
+*
+* This class contains all the test cases developed for NumberPrettifier
+* This test are included in the <pre>build.xml</pre> file, so will be 
+* executed each time the project is compiled.
+*
+* @author Carlos Carrascal Sanchez
+*
+*/
 public class NumberPrettifierTest {
 
+	/**
+	* Check output values supplied as examples 
+	*/
 	@Test
 	public void exampleEntriesShouldReturnAsExpected() {
 
-	    // MyClass is tested
 	    NumberPrettifier tester = new NumberPrettifier();
 
 	    // assert statements
@@ -18,13 +30,35 @@ public class NumberPrettifierTest {
 	    assertEquals("input: 2500000.34 should output: 2.5M", "2.5M", tester.format(2500000.34));
 	    assertEquals("input: 532 should output: 532", "532", tester.format(532));
 	    assertEquals("input: 1123456789 should output: 1.1B", "1.1B", tester.format(1123456789));
+	    assertEquals("input: 23000000000 should output: 23B", "23B", tester.format(23000000000d));
 
 	}
 
+	/**
+	* Check output values supplied as examples in negative values
+	*/
+	@Test
+	public void exampleEntriesWithNegativeValuesShouldReturnAsExpected() {
+
+	    NumberPrettifier tester = new NumberPrettifier();
+
+	    // assert statements
+	    assertEquals("input: -1000000 should output: -1M", "-1M", tester.format(-1000000));
+	    assertEquals("input: -2500000.34 should output: -2.5M", "-2.5M", tester.format(-2500000.34));
+	    assertEquals("input: -532 should output: -532", "-532", tester.format(-532));
+	    assertEquals("input: -1123456789 should output: -1.1B", "-1.1B", tester.format(-1123456789));
+	    assertEquals("input: 23000000000 should output: 23B", "23B", tester.format(23000000000d));
+
+	}
+
+	/**
+	* Check output values supplied as examples in Spanish locale. 
+	* Decimal character is expected to be "," instead of "."
+	*/
 	@Test
 	public void exampleEntriesInLocaleSpanishShouldReturnAsExpected() {
 
-	    // MyClass is tested
+	    // Contructed with another locale
 	    NumberPrettifier tester = new NumberPrettifier(new Locale("es", "ES"));
 
 	    // assert statements
@@ -32,9 +66,29 @@ public class NumberPrettifierTest {
 	    assertEquals("input: 2500000.34 should output: 2,5M", "2,5M", tester.format(2500000.34));
 	    assertEquals("input: 532 should output: 532", "532", tester.format(532));
 	    assertEquals("input: 1123456789 should output: 1,1B", "1,1B", tester.format(1123456789));
+	    assertEquals("input: 23000000000 should output: 23B", "23B", tester.format(23000000000d));
 
 	}
 
+	/**
+	* Check output values supplied as examples when a wrong locale is supplied
+	* Results should be the same as using Locale.US (default).
+	* No errors should be produced.
+	*/
+	@Test
+	public void exampleEntriesInWrongLocaleShouldReturnAsExpected() {
+
+	    // Contructed with wrong locale
+	    NumberPrettifier tester = new NumberPrettifier(new Locale("XX", "ZZ"));
+
+	    // assert statements
+	    assertEquals("input: 1000000 should output: 1M", "1M", tester.format(1000000));
+	    assertEquals("input: 2500000.34 should output: 2.5M", "2.5M", tester.format(2500000.34));
+	    assertEquals("input: 532 should output: 532", "532", tester.format(532));
+	    assertEquals("input: 1123456789 should output: 1.1B", "1.1B", tester.format(1123456789));
+	    assertEquals("input: 23000000000 should output: 23B", "23B", tester.format(23000000000d));
+
+	}
 
 
 }
