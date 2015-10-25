50b3f957f524d26332310069ea84d61a224f70a5
Charles <charles@totoro3>
2015-10-25 02:39:10 +0100

    Updated paths

--- Code/java_format_numbers/compile.sh
+++ Code/java_format_numbers/compile.sh
@@ -1,4 +1,6 @@
+# Compilation should be done using "ant", but this script will compile 
+# only the main class
 
-javac src/org/crossover/util/Prettifier.java  -d bin
+javac src/org/crossover/util/NumberPrettifier.java  -d bin
 
 
